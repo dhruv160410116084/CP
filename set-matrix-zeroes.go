1ef5cdbf3d840f92a3d2dae035b0311b88ea4925
dhruv <dhruv.160410116084@gmail.com>
2022-10-01 13:36:04 +0530

    optimized approact for set matrix zeroes added

--- set-matrix-zeroes.go
+++ set-matrix-zeroes.go
@@ -9,7 +9,8 @@ type void struct{}
 
 var member void
 
-func main() {
+// bruteforce approach
+func main1() {
 	matrix := [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}
 	// fmt.Println(matrix, len(matrix), len(matrix[0]))
 	rows := len(matrix)
@@ -47,4 +48,46 @@ func main() {
 		}
 	}
 	fmt.Println(matrix)
+}
+
+// optimized approach
+func main() {
+	// matrix := [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}
+	matrix := [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
+	fmt.Println("Current Matrix: ")
+	fmt.Println(matrix)
+
+	rows := len(matrix)
+	cols := len(matrix[0])
+	col0 := 1
+
+	for i := 0; i < rows; i++ {
+		if matrix[i][0] == 0 {
+			col0 = 0
+		}
+		for j := 1; j < cols; j++ {
+			if matrix[i][j] == 0 {
+				matrix[0][j], matrix[i][0] = 0, 0
+			}
+		}
+	}
+	fmt.Println("Updated Matrix: ")
+	fmt.Println(matrix)
+
+	for i := rows - 1; i > -1; i-- {
+		for j := cols - 1; j > 0; j-- {
+			fmt.Println(i, j, matrix[i][j])
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+
+		}
+		if col0 == 0 {
+			matrix[i][0] = 0
+		}
+	}
+	fmt.Println("Ans Matrix: ")
+
+	fmt.Println(matrix)
+
 }
