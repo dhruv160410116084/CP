acd70525d4e5c493165c6816abe24be0dac5df3d
dhruv <dhruv.160410116084@gmail.com>
2022-10-26 16:47:11 +0530

    longest-consecutive-subseq

---
+++ longest-consecutive-subsequence.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, -1}
+
+	result := 0
+	set := make(map[int]int)
+
+	for i, e := range nums {
+		set[e] = i + 1
+	}
+
+	for k, _ := range set {
+		if _, ok := set[k-1]; !ok {
+			ll := 1
+			// for _, next := set[k+1]; next && k < len(nums); k++ {
+			// 	// _, has := set[k+1]
+
+			// 	fmt.Println(set[k+1], next)
+			// 	ll++
+			// }
+			for set[k+1] != 0 {
+				ll++
+				k++
+			}
+			if ll > result {
+				result = ll
+			}
+		}
+	}
+	fmt.Println(result)
+
+}
